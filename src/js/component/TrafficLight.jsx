6be9f0e4866795bff62cde7548e275d66bcc418d
ManuuMT <ema14mt@gmail.com>
2021-11-24 03:11:49 +0000

    Project finished

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+	// Hooks
+	const [colour, setColour] = useState("");
+
+	// Methods
+	const changeColour = myColour => {
+		setColour(myColour);
+	};
+
+	// Every light it is a button
+	// The onClick function changes the state of the component
+	// The ternary operator adds the class that put the lights ON
+	return (
+		<div className="lights">
+			<button
+				onClick={() => changeColour("red")}
+				className={
+					"red" + (colour == "red" ? " red-ON " : "")
+				}></button>
+			<button
+				onClick={() => changeColour("yellow")}
+				className={
+					"yellow" + (colour == "yellow" ? " yellow-ON " : "")
+				}></button>
+			<button
+				onClick={() => changeColour("green")}
+				className={
+					"green" + (colour == "green" ? " green-ON " : "")
+				}></button>
+		</div>
+	);
+};
+
+export default TrafficLight;
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-//import react into the bundle
+//Import React
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
+//Import Bootstrap
 import "bootstrap";
 
-//include your index.scss file into the bundle
+//Styles
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
+//Components
+import TrafficLight from "./component/TrafficLight.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+//Render application
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
